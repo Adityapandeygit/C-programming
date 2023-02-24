d7e8f9ad7c29403e747c5e2084d8ecffd93d6bd0
Adityapandeygit <117390591+Adityapandeygit@users.noreply.github.com>
2023-02-24 22:25:08 +0530

    Add files via upload

---
+++ adding two number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int a,b,add;
+printf("enter first value");
+scanf("%d %d",&a,&b);
+
+
+add=a+b;
+printf("the total iss");
+printf(" sum %d\n",add);
+
+return 0;
+}
+
